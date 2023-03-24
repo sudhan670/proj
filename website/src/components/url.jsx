cf1799d552afce21ac7a3557b9e76c0929c4cbe9
SaravanakKumarTA <saravanakkumarta@gmail.com>
2023-03-24 10:50:30 +0530

    WVORENVKJ

--- website/src/components/url.jsx
+++ website/src/components/url.jsx
@@ -2,6 +2,6 @@ const urls = 0;
 export const endurl = urls
   ? "https://inventory-manager-70zs.onrender.com/app"
   : "http://localhost:4000/app";
-export const fronturl = urls
+export const fronturl = 1
   ? "https://inventory-manager-front.onrender.com/"
   : "http://localhost:3000";
