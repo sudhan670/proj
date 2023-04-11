f9e4d3e9b9f2de92840b6ba1425d279fc1d37fc8
SaravanakKumarTA <saravanakkumarta@gmail.com>
2023-04-11 16:28:24 +0530

    come commit

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -1,98 +1,106 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <div>
       <section id="#footer">
-<footer className="text-center text-lg-start bg-primary text-muted">
-  <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom text-white">
-    
-    <div className="me-5 d-none d-lg-block">
-      <span>Get connected with us on social networks:</span>
-    </div>
-    <div>
-      <a href="founder#" className="me-4 text-reset">
-        <i className="fa fa-facebook"></i>
-      </a>
-      <a href="foter#" className="me-4 text-reset">
-        <i className="fa fa-twitter"></i>
-      </a>
-      <a href="https://mail.google.com/mail/u/0/?ogbl#inbox/FMfcgzGslbHWHfPHnNxpBpFVFzQWJQSH?compose=new" className="me-4 text-reset">
-        <i className="fa fa-google"></i>
-      </a>
-      <a href="https://www.instagram.com/duraivg09/" className="me-4 text-reset">
-        <i className="fa fa-instagram"></i>
-      </a>
-      <a href="new#" className="me-4 text-reset">
-        <i className="fa fa-linkedin"></i>
-      </a>
-    </div>
-  
-  </section>
-  
+        <footer className="text-center text-lg-start bg-primary text-muted">
+          <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom text-white">
+            <div className="me-5 d-none d-lg-block">
+              <span>Get connected with us on social networks:</span>
+            </div>
+            <div>
+              <a href="founder#" className="me-4 text-reset">
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a href="foter#" className="me-4 text-reset">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a
+                href="https://mail.google.com/mail/u/0/?ogbl#inbox/FMfcgzGslbHWHfPHnNxpBpFVFzQWJQSH?compose=new"
+                className="me-4 text-reset"
+              >
+                <i className="fa fa-google"></i>
+              </a>
+              <a
+                href="https://www.instagram.com/duraivg09/"
+                className="me-4 text-reset"
+              >
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="new#" className="me-4 text-reset">
+                <i className="fa fa-linkedin"></i>
+              </a>
+            </div>
+          </section>
 
-  
-  <section id="#money" className="text-white">
-    <div className="container text-center text-md-start mt-5 text-white">
-      
-      <div className="row mt-3">
-        
-        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-          
-          <h6 className="text-uppercase fw-bold mb-4">
-            <i className="fas fa-gem me-3"></i>VGD TEXTILE MANUFACTURER 
-          </h6>
-          <p>
-            Thank You for Visting for Website with Your Valuable 
-          customers like You hope you will be happy with your family
-          </p>
-        </div>
-       
-        <div className="col-md-8 col-lg-8 col-xl-4 mx-auto mb-8">
-         
-          <h6 className="text-uppercase fw-bold mb-4">
-            About
-          </h6>
-          <p>
-            <a href="#!" className="text-reset">Pricing</a>
-          </p>
-          <p>
-            <a href="#!" className="text-reset">Settings</a>
-          </p>
-          <p>
-            <a href="#!" className="text-reset">Orders</a>
-          </p>
-          <p>
-            <a href="#!" className="text-reset">Help</a>
-          </p>
-        </div>
-        
-        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-       
-          <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-          <p><i className="fa fa-home me-3"></i>6/72C,Anna nagar,Idappadi,Salem</p>
-          <p>
-            <i className="fa fa-envelope me-3"></i>
-            vgdtexiles@gmail.com
-          </p>
-          <p><i className="fa fa-phone me-3"></i> +91 9488705946</p>
-          <p><i className="fa fa-print me-3"></i> +91 9842085946</p>
-        </div>
-    
-      </div>
-     
-    </div>
-  </section>
- 
-  <div className="text-center p-4 text-white">
-    © 2023 Copyright:
-    <a className="text-reset fw-bold" href="https://vgd.com/">vgdtexiles</a>
-  </div>
+          <section id="#money" className="text-white">
+            <div className="container text-center text-md-start mt-5 text-white">
+              <div className="row">
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">
+                    <i className="fas fa-gem me-3"></i>VGD TEXTILE MANUFACTURER
+                  </h6>
+                  <p>
+                    Thank You for Visting for Website with Your Valuable
+                    customers like You hope you will be happy with your family
+                  </p>
+                </div>
 
-</footer>
-</section>
-    </div>
-  )
-}
+                <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">About</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Pricing
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Settings
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Orders
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Help
+                    </a>
+                  </p>
+                </div>
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p>
+                    <i className="fa fa-home me-3"></i>6/72C,Anna
+                    nagar,Idappadi,Salem
+                  </p>
+                  <p>
+                    <i className="fa fa-envelope me-3"></i>
+                    vgdtexiles@gmail.com
+                  </p>
+                  <p>
+                    <i className="fa fa-phone me-3"></i> +91 9488705946
+                  </p>
+                  <p>
+                    <i className="fa fa-print me-3"></i> +91 9842085946
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
 
-export default Footer
+          <div className="text-center p-4 text-white">
+            © 2023 Copyright:
+            <a className="text-reset fw-bold" href="https://vgd.com/">
+              vgdtexiles
+            </a>
+          </div>
+        </footer>
+      </section>
+    </div>
+  );
+};
+
+export default Footer;
